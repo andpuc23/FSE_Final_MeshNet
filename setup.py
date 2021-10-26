a842d8c3ff2ad3045fddbd3c743fb5dff75bee90
roguLINA <48881806+roguLINA@users.noreply.github.com>
2021-10-26 21:28:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     version="1.1",
     packages=find_packages(),
     install_requires=[
-	"numpy~=1.21.2",
-	"torch~=1.10.0",
+	"numpy<=1.21.2",
+	"torch<=1.10.0",
 	"pymesh~=1.0.2",
-	"matplotlib~=3.4.2",
-	"PyYAML~=6.0",
+	"matplotlib<=3.4.2",
+	"PyYAML<=6.0",
 	"requests~=2.25.1"])
